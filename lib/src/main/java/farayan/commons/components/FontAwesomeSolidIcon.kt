e1012b8c27d446a571f4448715e6790903adaf59
Homayoun <behzadian@gmail.com>
2020-12-24 19:45:53 +0330

    init

---
+++ lib/src/main/java/farayan/commons/components/FontAwesomeSolidIcon.kt
@@ -0,0 +1,41 @@
+package farayan.commons.components
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.core.content.res.ResourcesCompat
+import farayan.commons.components.font.icon.awesome.solid.R
+import java.lang.Exception
+
+class FontAwesomeSolidIcon : FixedResourceFontTextView {
+
+    constructor(@androidx.annotation.NonNull context: Context) : super(context)
+    constructor(@androidx.annotation.NonNull context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        loadAttrs(attrs)
+    }
+
+    constructor(@androidx.annotation.NonNull context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        loadAttrs(attrs, defStyleAttr)
+    }
+
+
+    private fun loadAttrs(attrs: AttributeSet?, defStyleAttr: Int = 0) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.FontAwesomeSolidIcon, defStyleAttr, 0)
+        try {
+            val textCode = typedArray.getInt(R.styleable.FontAwesomeSolidIcon_SolidIcon, 0);
+            text = textCode.toChar().toString();
+            typeface = theFont(context, typefaceResID());
+        } finally {
+            typedArray.recycle();
+        }
+    }
+
+    override val inheritedFontResID: Int
+        get() = R.font.font_awesome_5_free_solid_900
+
+    override fun setTypeface(tf: Typeface?) {
+        if (tf != theFont(context, typefaceResID()))
+            return;
+        super.setTypeface(tf);
+    }
+}
